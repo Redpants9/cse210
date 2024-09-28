51d8bb36fd497f2ebc0dc95f533717f006772bfa
redpants9 <redpants97@gmailcom>
2024-09-28 16:19:38 -0600

    Well?

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,20 +7,4 @@ class Program
         Console.WriteLine("Hello Develop02 World!");
     }
 }
-public class Journal{
-    
-}
-public class Entry{
-    public string _date;
-    public string _promptText;
-    public string _entryText;
 
-    public void Display()
-    {
-        Console.WriteLine("");
-    }
-
-}
-public class PromptGenerator{
-
-}
