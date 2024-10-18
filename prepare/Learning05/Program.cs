978e8864fe2b1f4401873010a15b36aa9645a3c0
redpants9 <redpants97@gmailcom>
2024-10-18 01:51:22 -0600

    ok nevermind

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -9,7 +9,7 @@ class Program
 
         Mathassignment a2 = new Mathassignment("Heidi Hansen", "addition", ":section 1","1+1");
         Console.WriteLine(a2.Getsummary());
-        Console.WriteLine(a2.gethomeworklist);
+        Console.WriteLine(a2.gethomeworklist());
 
         Writingassignment a3 = new Writingassignment("Neff Hansen", "esaay", "many cool things");
         Console.WriteLine(a3.Getsummary());
