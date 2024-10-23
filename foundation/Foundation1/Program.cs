1accd3689ea454af52a59b1d69ae643b473e9917
redpants9 <redpants97@gmailcom>
2024-10-23 01:21:24 -0600

    ok then

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,11 +4,16 @@ class Program
     {
         Video vid1 = new Video("a person", "John", 103);
         vid1.fullvid();
+        Comment com1 = new Comment("a guy", "First");
+        com1.display();
 
         Video vid2 = new Video("How I lost to a thing", "Bryan", 256);
         vid2.fullvid();
+        Comment com2 = new Comment("Jake", "hello");
+        com2.display();
 
         Video vid3 = new Video("I'M SORRY", "apologetic dude", 25);
         vid3.fullvid();
+        Comment com3 = new Comment("John", "Ok a i guess");
     }
 }
